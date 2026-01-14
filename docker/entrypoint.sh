130393a931cf217abb0acbcf984a5e2104de0be0
Hentry Martin <hentrymartin.p@gmail.com>
2026-01-14 21:36:40 +0530

    feat: run prisma migrations during deployment

--- docker/entrypoint.sh
+++ docker/entrypoint.sh
@@ -2,7 +2,7 @@
 set -e
 
 echo "Running Prisma migrations..."
-npx prisma migrate deploy --schema=prisma/schema.prisma
+yarn prisma migrate deploy --schema=prisma/schema.prisma
 
 echo "Starting application..."
 exec node app.js
