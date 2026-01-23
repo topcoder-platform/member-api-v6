1c698e1b14be9d85dc89aca240e6195e35b76a28
Hentry Martin <hentrymartin.p@gmail.com>
2026-01-23 20:00:37 +0530

    fix: remove fallback urls

--- config/default.js
+++ config/default.js
@@ -127,7 +127,7 @@ module.exports = {
   MEMBER_SERVICE_PRISMA_TIMEOUT: process.env.MEMBER_SERVICE_PRISMA_TIMEOUT ? parseInt(process.env.MEMBER_SERVICE_PRISMA_TIMEOUT, 10) : 10000,
 
   // Finance database connection string
-  FINANCE_DATABASE_URL: process.env.FINANCE_DATABASE_URL || process.env.DATABASE_URL,
+  FINANCE_DATABASE_URL: process.env.FINANCE_DATABASE_URL,
 
   HUBSPOT_API_KEY: process.env.HUBSPOT_API_KEY,
   HUBSPOT_BASE_URL: process.env.HUBSPOT_BASE_URL,
